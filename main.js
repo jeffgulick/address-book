464318288e7207d63e788740832e9ad9aaf1b60c
JEFFGULICK <jeffgulick@gmail.com>
2020-08-05 02:54:09 -0500

    mostly finished. still adding bootstrap collapse for hidden info

--- main.js
+++ main.js
@@ -5,19 +5,37 @@ window.onload = function() {
   
   }
 const getUser = () => {
-    fetch('https://randomuser.me/api/')
+    fetch('https://randomuser.me/api/?results=5')
       .then(res => res.json())
       .then(data => {
-          const user = data.results[0]
-          console.log(user);
-          const allUsers = document.getElementById('user-entries');
-          const li = document.createElement('li');
-          const image = document.createElement('img');
-          image.setAttribute('src', user.picture.thumbnail);
-          const text = document.createTextNode(`Name: ${user.name.first} `)
-          li.appendChild(text)
-          li.appendChild(image)
-          allUsers.append(li)
+          addressUser = data.results;
+
+          addressUser.map(user => {
+            let allUsers = document.getElementById('user-entries');
+            let li = document.createElement('li');
+            let image = document.createElement('img');
+            image.setAttribute('src', user.picture.thumbnail);
+            // button.setAttribute('onclick', expand() )
+
+            let hidden = document.createElement('div');
+            hidden.classList.add('collapse');
+            let hiddenTxt = document.createTextNode(`Email: ${user.email}`);
+            hidden.appendChild(hiddenTxt);
+
+            let button = document.createElement('button')
+            let text = document.createTextNode(`Name: ${user.name.first}  ${user.name.last} `);
+            let buttonText = document.createTextNode('button')
+            button.appendChild(buttonText);
+
+
+            li.appendChild(text);
+            li.appendChild(image);
+            li.appendChild(button);
+            li.appendChild(hidden);
+            allUsers.append(li);
+          })
+
+          console.log(addressUser)
       }) 
   }
 
